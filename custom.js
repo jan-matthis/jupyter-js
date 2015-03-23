4bfb6e97090efa72ea3f2d8067031f1515f78c90
jan-matthis <post@jan-matthis.de>
2015-03-23 21:37:43 +0100

    Update custom.js

--- custom.js
+++ custom.js
@@ -1,68 +1,68 @@
 /* shortcuts */
 
-require(["base/js/events"], function (events) { events.on("notebook_loaded.Notebook", function () {
+require(["base/js/events"], function (events) { 
+        events.on("notebook_loaded.Notebook", function () {
 
-        // toggle notebook width
-        IPython.keyboard_manager.command_shortcuts.add_shortcut("Ctrl-Shift-f", {
-            help : 'Toggle container width',
-            help_index : 'zzz',
-            handler : function(e) { 
-                
-                //console.log('Toggle Container Width');
+                // toggle notebook width
+                IPython.keyboard_manager.command_shortcuts.add_shortcut("Ctrl-Shift-f", {
+                    help : 'Toggle container width',
+                    help_index : 'zzz',
+                    handler : function(e) { 
+                        
+                        //console.log('Toggle Container Width');
+        
+                        var currentWidth = $('.container').css('width');
+                        var newWidth = '100%';
+                        if(null == window.originalWidth) {
+                            window.originalWidth = currentWidth;
+                        } else {
+                            if(currentWidth !== window.originalWidth) {
+                                newWidth = window.originalWidth;
+                            }
+                        }
+                        $('.container').css('width',newWidth);
+                        
+                        return false;
+                    }}
+                );
+        
+        
+        
+                // toggle code (hide/show)
+                IPython.keyboard_manager.command_shortcuts.add_shortcut("Alt-t", {
+                    help : 'Toggle code',
+                    help_index : 'zzz',
+                    handler : function(e) { 
+                        $('div.input').toggle();
+                        return false;
+                    }}
+                );
+        
+        
+        
+                // clear all output
+                IPython.keyboard_manager.command_shortcuts.add_shortcut("Ctrl-x", {
+                    help : 'Clear all output',
+                    help_index : 'zzz',
+                    handler : function(e) { 
+                        IPython.notebook.clear_all_output();
+                        return false;
+                    }}
+                );
+        
+        
+        
+                // open qt console
+                IPython.keyboard_manager.command_shortcuts.add_shortcut("Shift-q", {
+                    help : 'Open QT Console',
+                    help_index : 'zzz',
+                    handler : function(e) { 
+                        IPython.notebook.kernel.execute('%qtconsole');
+                        return false;
+                    }}
+                );
 
-                var currentWidth = $('.container').css('width');
-                var newWidth = '100%';
-                if(null == window.originalWidth) {
-                    window.originalWidth = currentWidth;
-                } else {
-                    if(currentWidth !== window.originalWidth) {
-                        newWidth = window.originalWidth;
-                    }
-                }
-                $('.container').css('width',newWidth);
-                
-                return false;
-            }}
-        );
-
-
-
-        // toggle code (hide/show)
-        IPython.keyboard_manager.command_shortcuts.add_shortcut("Alt-t", {
-            help : 'Toggle code',
-            help_index : 'zzz',
-            handler : function(e) { 
-                $('div.input').toggle();
-                return false;
-            }}
-        );
-
-
-
-        // clear all output
-        IPython.keyboard_manager.command_shortcuts.add_shortcut("Ctrl-x", {
-            help : 'Clear all output',
-            help_index : 'zzz',
-            handler : function(e) { 
-                IPython.notebook.clear_all_output();
-                return false;
-            }}
-        );
-
-
-
-        // open qt console
-        IPython.keyboard_manager.command_shortcuts.add_shortcut("Shift-q", {
-            help : 'Open QT Console',
-            help_index : 'zzz',
-            handler : function(e) { 
-                IPython.notebook.kernel.execute('%qtconsole');
-                return false;
-            }}
-        );
-
-})
-
+        })
 });
 
 
