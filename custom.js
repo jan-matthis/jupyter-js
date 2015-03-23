c40a0a071b3c735afa2928ce6bdbdcdfa6555540
jan-matthis <post@jan-matthis.de>
2015-03-23 10:07:49 +0100

    wrapping shortcut code into on init fun

--- custom.js
+++ custom.js
@@ -1,64 +1,67 @@
 /* shortcuts */
 
-// toggle notebook width
-IPython.keyboard_manager.command_shortcuts.add_shortcut("Ctrl-Shift-f", {
-    help : 'Toggle container width',
-    help_index : 'zzz',
-    handler : function(e) { 
-        
-        //console.log('Toggle Container Width');
+$([IPython.events]).on('app_initialized.NotebookApp', function(){
 
-        var currentWidth = $('.container').css('width');
-        var newWidth = '100%';
-        if(null == window.originalWidth) {
-            window.originalWidth = currentWidth;
-        } else {
-            if(currentWidth !== window.originalWidth) {
-                newWidth = window.originalWidth;
-            }
-        }
-        $('.container').css('width',newWidth);
-        
-        return false;
-    }}
-);
+        // toggle notebook width
+        IPython.keyboard_manager.command_shortcuts.add_shortcut("Ctrl-Shift-f", {
+            help : 'Toggle container width',
+            help_index : 'zzz',
+            handler : function(e) { 
+                
+                //console.log('Toggle Container Width');
 
-
-
-// toggle code (hide/show)
-IPython.keyboard_manager.command_shortcuts.add_shortcut("Alt-t", {
-    help : 'Toggle code',
-    help_index : 'zzz',
-    handler : function(e) { 
-        $('div.input').toggle();
-        return false;
-    }}
-);
+                var currentWidth = $('.container').css('width');
+                var newWidth = '100%';
+                if(null == window.originalWidth) {
+                    window.originalWidth = currentWidth;
+                } else {
+                    if(currentWidth !== window.originalWidth) {
+                        newWidth = window.originalWidth;
+                    }
+                }
+                $('.container').css('width',newWidth);
+                
+                return false;
+            }}
+        );
 
 
 
-// clear all output
-IPython.keyboard_manager.command_shortcuts.add_shortcut("Ctrl-x", {
-    help : 'Clear all output',
-    help_index : 'zzz',
-    handler : function(e) { 
-        IPython.notebook.clear_all_output();
-        return false;
-    }}
-);
+        // toggle code (hide/show)
+        IPython.keyboard_manager.command_shortcuts.add_shortcut("Alt-t", {
+            help : 'Toggle code',
+            help_index : 'zzz',
+            handler : function(e) { 
+                $('div.input').toggle();
+                return false;
+            }}
+        );
 
 
 
-// open qt console
-IPython.keyboard_manager.command_shortcuts.add_shortcut("Shift-q", {
-    help : 'Open QT Console',
-    help_index : 'zzz',
-    handler : function(e) { 
-        IPython.notebook.kernel.execute('%qtconsole');
-        return false;
-    }}
-);
+        // clear all output
+        IPython.keyboard_manager.command_shortcuts.add_shortcut("Ctrl-x", {
+            help : 'Clear all output',
+            help_index : 'zzz',
+            handler : function(e) { 
+                IPython.notebook.clear_all_output();
+                return false;
+            }}
+        );
 
+
+
+        // open qt console
+        IPython.keyboard_manager.command_shortcuts.add_shortcut("Shift-q", {
+            help : 'Open QT Console',
+            help_index : 'zzz',
+            handler : function(e) { 
+                IPython.notebook.kernel.execute('%qtconsole');
+                return false;
+            }}
+        );
+
+});
 
 
 /* mathjax */
